7e9a290d8837231b12f06162d93862dc508c8041
Achiel <achiel.van.der.mandele@mendix.com>
2015-07-09 14:44:57 +0200

    Refresh on entity if no context provided
    Old widget apparently always worked from a dataview and listened to that
    object, so it was logical to only listen to one obj. Current widget
    assumes xpaths more than context, so it makes more sense to listen to an
    entity (at least when no context is available).

--- src/GoogleMaps/widget/GoogleMaps.js
+++ src/GoogleMaps/widget/GoogleMaps.js
@@ -62,6 +62,15 @@ define([
                     })
                 });
             }
+            else {
+                this._handle = this.subscribe({
+                    entity: this.mapEntity,
+                    callback: lang.hitch(this, function (entity) {
+                        this._fetchMarkers();
+                    })
+                });
+
+            }
         },
 
         _loadMap: function () {
